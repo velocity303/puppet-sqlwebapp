a52c6f68944d6eff22673000349f4e7343d3f0f1
James E. Jones <james.jones@puppetlabs.com>
2015-03-24 16:05:19 -0500

    add iis class to webapp class and remove staging module requirement

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,27 +3,31 @@ class sqlwebapp (
   $docroot       = 'C:/inetpub/wwwroot',
   $db_instance   = 'MYINSTANCE',
   $iis_site      = 'Default Web Site',
-  $file_source   = 'http://master.inf.puppetlabs.demo',
+  $file_source   = 'puppet:///modules/sqlwebapp/',
   $db_password   = 'Azure$123',
   $webapp_zip    = 'CloudShop.zip',
   $webapp_name   = 'CloudShop',
   $webapp_config = 'Web.config',
 ) {
+  require sqlwebapp::iis
   file { "${docroot}/${webapp_name}":
     ensure  => directory,
-    require => Class['tse_sqlserver::iisdb'],
   }
-  staging::deploy { $webapp_zip:
-    target  => "${docroot}/${webapp_name}",
-    creates => "${docroot}/${webapp_name}/${webapp_config}",
-    source  => "${file_source}/${webapp_zip}",
-    require => File["${docroot}/${webapp_name}"],
-    notify  => Exec['ConvertAPP'],
+  file { "${docroot}/${webapp_zip}":
+    ensure => present,
+    source => "${file_source}/${webapp_zip}",
+  }
+  unzip { "Unzip webapp ${webapp_zip}":
+    source      => "${path}/${zip_file}",
+    creates     => "${docroot}/${webapp_name}/${webapp_config}",
+    destination => "${docroot}/${webapp_name}",
+    require     => File["${docroot}/${webapp_zip}"],
+    notify      => Exec['ConvertApp'],
   }
   file { "${docroot}/${webapp_name}/${webapp_config}":
     ensure  => present,
     content => template("${module_name}/Web.config.erb"),
-    require => Staging::Deploy[$webapp_zip],
+    require => Unzip["Unzip webapp ${webapp_zip}"],
   }
   exec { 'ConvertAPP':
     command     => "ConvertTo-WebApplication \'IIS:/Sites/${iis_site}/${webapp_name}\'",
