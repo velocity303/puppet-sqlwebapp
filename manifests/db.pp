6dfba63e25767d16fc4536df1971dfe629445902
James Jones <velocity303@gmail.com>
2015-12-20 21:15:42 -0600

    update file source for db

--- manifests/db.pp
+++ manifests/db.pp
@@ -2,9 +2,10 @@ class sqlwebapp::db (
   $dbinstance    = 'MYINSTANCE',
   $dbpass        = 'Azure$123',
   $dbuser        = 'CloudShop',
+  $file_source   = 'https://s3-us-west-2.amazonaws.com/tseteam/files/sqlwebapp',
 ){
   tse_sqlserver::attachdb { 'AdventureWorks2012':
-    file_source => 'https://s3-us-west-2.amazonaws.com/tseteam/files/tse_sqlserver'
+    file_source => $file_source,
     dbinstance  => $dbinstance,
     dbpass      => $dbpass,
     dbuser      => $dbuser,
