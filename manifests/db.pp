25aa530ead1b9242acbd04f561618a5a9e03aaba
James Jones <velocity303@gmail.com>
2015-12-20 22:37:04 -0600

    update parameters and dbname variable

--- manifests/db.pp
+++ manifests/db.pp
@@ -2,9 +2,11 @@ class sqlwebapp::db (
   $dbinstance    = 'MYINSTANCE',
   $dbpass        = 'Azure$123',
   $dbuser        = 'CloudShop',
+  $dbname        = 'AdventureWorks2012',
+  $dbserver      = $::fqdn,
   $file_source   = 'https://s3-us-west-2.amazonaws.com/tseteam/files/sqlwebapp',
 ){
-  tse_sqlserver::attachdb { 'AdventureWorks2012':
+  tse_sqlserver::attachdb { $dbname:
     file_source => $file_source,
     dbinstance  => $dbinstance,
     dbpass      => $dbpass,
